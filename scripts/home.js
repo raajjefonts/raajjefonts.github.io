6130f5934b6081a8597ad46cef610dc4c90250e7
raajjefonts <132810670+raajjefonts@users.noreply.github.com>
2023-05-08 18:37:37 +0500

    Add files via upload

---
+++ scripts/home.js
@@ -0,0 +1,475 @@
+// listings
+var fontsListing = [
+{
+name: "Athu Casual",
+file_name: "Athu_Casual.otf",
+web_file_location: "fonts/Athu_Casual.otf",
+download_file_location: "fonts/Athu_Casual.otf",
+download_font_size: "19 KB"
+},
+{
+name: "Avas Thaana",
+file_name: "Avas_Thaana.otf",
+web_file_location: "fonts/Avas_Thaana.otf",
+download_file_location: "fonts/Avas_Thaana.otf",
+download_font_size: "15 KB"
+},
+{
+name: "A Haleem Bolhu  Bold",
+file_name: "A_Haleem_Bolhu__Bold.otf",
+web_file_location: "fonts/A_Haleem_Bolhu__Bold.otf",
+download_file_location: "fonts/A_Haleem_Bolhu__Bold.otf",
+download_font_size: "17 KB"
+},
+{
+name: "A Haleem faseyha",
+file_name: "A_Haleem_faseyha.otf",
+web_file_location: "fonts/A_Haleem_faseyha.otf",
+download_file_location: "fonts/A_Haleem_faseyha.otf",
+download_font_size: "37 KB"
+},
+{
+name: "A Haleem KB",
+file_name: "A_Haleem_KB.otf",
+web_file_location: "fonts/A_Haleem_KB.otf",
+download_file_location: "fonts/A_Haleem_KB.otf",
+download_font_size: "15 KB"
+},
+{
+name: "A Haleem Mathi Bold",
+file_name: "A_Haleem_Mathi_Bold.otf",
+web_file_location: "fonts/A_Haleem_Mathi_Bold.otf",
+download_file_location: "fonts/A_Haleem_Mathi_Bold.otf",
+download_font_size: "14 KB"
+},
+{
+name: "A Haleem Raaveri",
+file_name: "A_Haleem_Raaveri.otf",
+web_file_location: "fonts/A_Haleem_Raaveri.otf",
+download_file_location: "fonts/A_Haleem_Raaveri.otf",
+download_font_size: "28 KB"
+},
+{
+name: "A Haleem Riveli",
+file_name: "A_Haleem_Riveli.otf",
+web_file_location: "fonts/A_Haleem_Riveli.otf",
+download_file_location: "fonts/A_Haleem_Riveli.otf",
+download_font_size: "9 KB"
+},
+{
+name: "A Haleem SH Bold Italic",
+file_name: "A_Haleem_SH_Bold_Italic.otf",
+web_file_location: "fonts/A_Haleem_SH_Bold_Italic.otf",
+download_file_location: "fonts/A_Haleem_SH_Bold_Italic.otf",
+download_font_size: "11 KB"
+},
+{
+name: "A Haleem Thangi Bold",
+file_name: "A_Haleem_Thangi_Bold.otf",
+web_file_location: "fonts/A_Haleem_Thangi_Bold.otf",
+download_file_location: "fonts/A_Haleem_Thangi_Bold.otf",
+download_font_size: "21 KB"
+},
+{
+name: "A Haleem Thiki Bold",
+file_name: "A_Haleem_Thiki_Bold.otf",
+web_file_location: "fonts/A_Haleem_Thiki_Bold.otf",
+download_file_location: "fonts/A_Haleem_Thiki_Bold.otf",
+download_font_size: "46 KB"
+},
+{
+name: "A Haleem Uivashaa",
+file_name: "A_Haleem_Uivashaa.otf",
+web_file_location: "fonts/A_Haleem_Uivashaa.otf",
+download_file_location: "fonts/A_Haleem_Uivashaa.otf",
+download_font_size: "29 KB"
+},
+{
+name: "A Haleem  Kirudhooni",
+file_name: "A_Haleem__Kirudhooni.otf",
+web_file_location: "fonts/A_Haleem__Kirudhooni.otf",
+download_file_location: "fonts/A_Haleem__Kirudhooni.otf",
+download_font_size: "11 KB"
+},
+{
+name: "A Ilham",
+file_name: "A_Ilham.TTF",
+web_file_location: "fonts/A_Ilham.TTF",
+download_file_location: "fonts/A_Ilham.TTF",
+download_font_size: "24 KB"
+},
+{
+name: "A Kaani",
+file_name: "A_Kaani.otf",
+web_file_location: "fonts/A_Kaani.otf",
+download_file_location: "fonts/A_Kaani.otf",
+download_font_size: "13 KB"
+},
+{
+name: "A Koagannu",
+file_name: "A_Koagannu.TTF",
+web_file_location: "fonts/A_Koagannu.TTF",
+download_file_location: "fonts/A_Koagannu.TTF",
+download_font_size: "25 KB"
+},
+{
+name: "A Lakudi",
+file_name: "A_Lakudi.otf",
+web_file_location: "fonts/A_Lakudi.otf",
+download_file_location: "fonts/A_Lakudi.otf",
+download_font_size: "12 KB"
+},
+{
+name: "A Lakudi College",
+file_name: "A_Lakudi_College.otf",
+web_file_location: "fonts/A_Lakudi_College.otf",
+download_file_location: "fonts/A_Lakudi_College.otf",
+download_font_size: "35 KB"
+},
+{
+name: "A Midhili",
+file_name: "A_Midhili.otf",
+web_file_location: "fonts/A_Midhili.otf",
+download_file_location: "fonts/A_Midhili.otf",
+download_font_size: "13 KB"
+},
+{
+name: "A Uni",
+file_name: "A_Uni.otf",
+web_file_location: "fonts/A_Uni.otf",
+download_file_location: "fonts/A_Uni.otf",
+download_font_size: "13 KB"
+},
+{
+name: "A Waheed",
+file_name: "A_Waheed.otf",
+web_file_location: "fonts/A_Waheed.otf",
+download_file_location: "fonts/A_Waheed.otf",
+download_font_size: "65 KB"
+},
+{
+name: "A Waheed",
+file_name: "A_Waheed.ttf",
+web_file_location: "fonts/A_Waheed.ttf",
+download_file_location: "fonts/A_Waheed.ttf",
+download_font_size: "24 KB"
+},
+{
+name: "A Waheed College",
+file_name: "A_Waheed_College.otf",
+web_file_location: "fonts/A_Waheed_College.otf",
+download_file_location: "fonts/A_Waheed_College.otf",
+download_font_size: "63 KB"
+},
+{
+name: "BODUKURU Light",
+file_name: "BODUKURU_Light.otf",
+web_file_location: "fonts/BODUKURU_Light.otf",
+download_file_location: "fonts/BODUKURU_Light.otf",
+download_font_size: "20 KB"
+},
+{
+name: "DAM Hiyani",
+file_name: "DAM_Hiyani.otf",
+web_file_location: "fonts/DAM_Hiyani.otf",
+download_file_location: "fonts/DAM_Hiyani.otf",
+download_font_size: "29 KB"
+},
+{
+name: "DAM Kalhi",
+file_name: "DAM_Kalhi.otf",
+web_file_location: "fonts/DAM_Kalhi.otf",
+download_file_location: "fonts/DAM_Kalhi.otf",
+download_font_size: "24 KB"
+},
+{
+name: "DAM Kathivalhi",
+file_name: "DAM_Kathivalhi.otf",
+web_file_location: "fonts/DAM_Kathivalhi.otf",
+download_file_location: "fonts/DAM_Kathivalhi.otf",
+download_font_size: "16 KB"
+},
+{
+name: "DAM Madheeh",
+file_name: "DAM_Madheeh.otf",
+web_file_location: "fonts/DAM_Madheeh.otf",
+download_file_location: "fonts/DAM_Madheeh.otf",
+download_font_size: "18 KB"
+},
+{
+name: "Dhivehi",
+file_name: "Dhivehi.ttf",
+web_file_location: "fonts/Dhivehi.ttf",
+download_file_location: "fonts/Dhivehi.ttf",
+download_font_size: "23 KB"
+},
+{
+name: "Dhives",
+file_name: "Dhives.ttf",
+web_file_location: "fonts/Dhives.ttf",
+download_file_location: "fonts/Dhives.ttf",
+download_font_size: "23 KB"
+},
+{
+name: "Faiy Light",
+file_name: "Faiy_Light.otf",
+web_file_location: "fonts/Faiy_Light.otf",
+download_file_location: "fonts/Faiy_Light.otf",
+download_font_size: "35 KB"
+},
+{
+name: "Faruma",
+file_name: "Faruma.otf",
+web_file_location: "fonts/Faruma.otf",
+download_file_location: "fonts/Faruma.otf",
+download_font_size: "78 KB"
+},
+{
+name: "midhilibold",
+file_name: "midhilibold.ttf",
+web_file_location: "fonts/midhilibold.ttf",
+download_file_location: "fonts/midhilibold.ttf",
+download_font_size: "76 KB"
+},
+{
+name: "MVWaheed",
+file_name: "MVWaheed.otf",
+web_file_location: "fonts/MVWaheed.otf",
+download_file_location: "fonts/MVWaheed.otf",
+download_font_size: "21 KB"
+},
+{
+name: "Mv Amaan XP",
+file_name: "Mv_Amaan_XP.otf",
+web_file_location: "fonts/Mv_Amaan_XP.otf",
+download_file_location: "fonts/Mv_Amaan_XP.otf",
+download_font_size: "21 KB"
+},
+{
+name: "MV Boli",
+file_name: "MV_Boli.otf",
+web_file_location: "fonts/MV_Boli.otf",
+download_file_location: "fonts/MV_Boli.otf",
+download_font_size: "43 KB"
+},
+{
+name: "Mv Eamaan XP",
+file_name: "Mv_Eamaan_XP.otf",
+web_file_location: "fonts/Mv_Eamaan_XP.otf",
+download_file_location: "fonts/Mv_Eamaan_XP.otf",
+download_font_size: "20 KB"
+},
+{
+name: "Mv Elaaf Lite",
+file_name: "Mv_Elaaf_Lite.otf",
+web_file_location: "fonts/Mv_Elaaf_Lite.otf",
+download_file_location: "fonts/Mv_Elaaf_Lite.otf",
+download_font_size: "20 KB"
+},
+{
+name: "Mv Elaaf Normal",
+file_name: "Mv_Elaaf_Normal.otf",
+web_file_location: "fonts/Mv_Elaaf_Normal.otf",
+download_file_location: "fonts/Mv_Elaaf_Normal.otf",
+download_font_size: "20 KB"
+},
+{
+name: "MV Faseyha",
+file_name: "MV_Faseyha.otf",
+web_file_location: "fonts/MV_Faseyha.otf",
+download_file_location: "fonts/MV_Faseyha.otf",
+download_font_size: "25 KB"
+},
+{
+name: "Mv Galan Normal",
+file_name: "Mv_Galan_Normal.otf",
+web_file_location: "fonts/Mv_Galan_Normal.otf",
+download_file_location: "fonts/Mv_Galan_Normal.otf",
+download_font_size: "27 KB"
+},
+{
+name: "Mv GroupX Avas Akuru",
+file_name: "Mv_GroupX_Avas_Akuru.otf",
+web_file_location: "fonts/Mv_GroupX_Avas_Akuru.otf",
+download_file_location: "fonts/Mv_GroupX_Avas_Akuru.otf",
+download_font_size: "25 KB"
+},
+{
+name: "Mv Iyyu Formal",
+file_name: "Mv_Iyyu_Formal.otf",
+web_file_location: "fonts/Mv_Iyyu_Formal.otf",
+download_file_location: "fonts/Mv_Iyyu_Formal.otf",
+download_font_size: "16 KB"
+},
+{
+name: "Mv Iyyu Nala",
+file_name: "Mv_Iyyu_Nala.otf",
+web_file_location: "fonts/Mv_Iyyu_Nala.otf",
+download_file_location: "fonts/Mv_Iyyu_Nala.otf",
+download_font_size: "20 KB"
+},
+{
+name: "Mv Iyyu Normal",
+file_name: "Mv_Iyyu_Normal.otf",
+web_file_location: "fonts/Mv_Iyyu_Normal.otf",
+download_file_location: "fonts/Mv_Iyyu_Normal.otf",
+download_font_size: "20 KB"
+},
+{
+name: "Mv Lady Luck",
+file_name: "Mv_Lady_Luck.otf",
+web_file_location: "fonts/Mv_Lady_Luck.otf",
+download_file_location: "fonts/Mv_Lady_Luck.otf",
+download_font_size: "23 KB"
+},
+{
+name: "Mv MAG Round Bold",
+file_name: "Mv_MAG_Round_Bold.otf",
+web_file_location: "fonts/Mv_MAG_Round_Bold.otf",
+download_file_location: "fonts/Mv_MAG_Round_Bold.otf",
+download_font_size: "22 KB"
+},
+{
+name: "Mv MAG Round HBold",
+file_name: "Mv_MAG_Round_HBold.otf",
+web_file_location: "fonts/Mv_MAG_Round_HBold.otf",
+download_file_location: "fonts/Mv_MAG_Round_HBold.otf",
+download_font_size: "32 KB"
+},
+{
+name: "Mv MAG Round XBold",
+file_name: "Mv_MAG_Round_XBold.otf",
+web_file_location: "fonts/Mv_MAG_Round_XBold.otf",
+download_file_location: "fonts/Mv_MAG_Round_XBold.otf",
+download_font_size: "20 KB"
+},
+{
+name: "Mv Sehga Fubaru Fancy",
+file_name: "Mv_Sehga_Fubaru_Fancy.otf",
+web_file_location: "fonts/Mv_Sehga_Fubaru_Fancy.otf",
+download_file_location: "fonts/Mv_Sehga_Fubaru_Fancy.otf",
+download_font_size: "21 KB"
+},
+{
+name: "Mv Sehga Old",
+file_name: "Mv_Sehga_Old.otf",
+web_file_location: "fonts/Mv_Sehga_Old.otf",
+download_file_location: "fonts/Mv_Sehga_Old.otf",
+download_font_size: "22 KB"
+},
+{
+name: "MV Waheed",
+file_name: "MV_Waheed.otf",
+web_file_location: "fonts/MV_Waheed.otf",
+download_file_location: "fonts/MV_Waheed.otf",
+download_font_size: "21 KB"
+},
+{
+name: "Nasr Light",
+file_name: "Nasr_Light.otf",
+web_file_location: "fonts/Nasr_Light.otf",
+download_file_location: "fonts/Nasr_Light.otf",
+download_font_size: "31 KB"
+},
+{
+name: "Samee Avas Thaana",
+file_name: "Samee_Avas_Thaana.ttf",
+web_file_location: "fonts/Samee_Avas_Thaana.ttf",
+download_file_location: "fonts/Samee_Avas_Thaana.ttf",
+download_font_size: "48 KB"
+}
+];
+
+
+// actual processing 
+
+var testText = "";
+var testTextInput = document.getElementById("test");
+testText = testTextInput.value;
+
+
+if (window.localStorage.getItem('RaajjeFontsTestText')) {
+		testTextInput.value = window.localStorage.getItem('RaajjeFontsTestText');
+		testText = window.localStorage.getItem('RaajjeFontsTestText');
+}
+
+
+displayListings();
+
+function updateTestText() {
+	testText = testTextInput.value;
+	window.localStorage.setItem('RaajjeFontsTestText', testText);
+	console.log(testText);
+	removeAllListings();
+	displayListings();
+}
+
+function removeAllListings() {
+	var listings_box = document.getElementById("listings-box");
+	listings_box.innerHTML = "";
+}
+
+function displayListings() {
+	for (var elisting of fontsListing) {
+		/*
+		var listing = document.getElementById("listing");
+		var listing_name = document.getElementById("listing_name");
+		var listing_display = document.getElementById("listing_display");
+		var listing_file_name = document.getElementById("listing_file_name");
+		var listing_size = document.getElementById("listing_size");
+		var listing_download = document.getElementById("listing_download");
+		*/
+		
+		var listing_name = document.createElement("p");
+		listing_name.className = "listing-name";
+		listing_name.innerText = elisting.name;
+		
+		var style = document.createElement('style');
+		style.type = 'text/css';
+		style.innerHTML = '@font-face { font-family: "'+elisting.name+'"; src: url("'+elisting.web_file_location+'"); }';
+		document.head.appendChild(style);
+		
+		var listing_display = document.createElement("p");
+		listing_display.className = "listing-display";
+		listing_display.innerText = testText;
+		listing_display.style.fontFamily = elisting.name;
+		
+		var listing_file_name = document.createElement("p");
+		listing_file_name.className = "listing-file-name";
+		listing_file_name.innerText = "file: "+elisting.file_name;
+		
+		var listing_size = document.createElement("p");
+		listing_size.className = "listing-size";
+		listing_size.innerText = "size: "+elisting.download_font_size;
+		
+					var listing_down_text = document.createElement("p");
+					listing_down_text.innerText = "Download";
+				
+				var listing_download = document.createElement("div");
+				listing_download.className = "listing-download";
+				listing_download.appendChild(listing_down_text);
+			
+			var download_a = document.createElement("a");
+			download_a.appendChild(listing_download);
+			download_a.href = elisting.download_file_location;
+		
+		var horizontal = document.createElement("div");
+		horizontal.className = "horizontal";
+		horizontal.appendChild(listing_size);
+		horizontal.appendChild(download_a);
+		
+		var listing = document.createElement("div");
+		listing.className = "listing";
+		
+		listing.appendChild(listing_name);
+		listing.appendChild(listing_display);
+		listing.appendChild(listing_file_name);
+		listing.appendChild(horizontal);
+		
+		var listings_box = document.getElementById("listings-box");
+		listings_box.append(listing);
+
+		
+		
+	}
+}
